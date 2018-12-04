d02fcc9580eacb1a36e56b4b5bce067a396b9c62
Alan Guegan <alanguegan@pop-os.localdomain>
2018-12-04 14:34:19 +0100

    correction

---
+++ Connection_BDD/test.createUser.js
@@ -0,0 +1,10 @@
+const createUser = require('./createUser');
+describe('Create User', function() {
+  describe('Create user success', function() {
+    
+    it('should return true when the user is create', function() {
+      expect.assertions(1);
+      expect("").toEqual("");
+    });
+  });
+});
