1fb1c4be7b0a94e44df7799b745624c461491297
Alan Guegan <alanguegan@pop-os.localdomain>
2018-12-04 14:00:25 +0100

    creation of a component that will manage the connection to the database
    Component managing the database without the user knowing which database is used or how it is structured

---
+++ Connection_BDD/test/testCreateUser.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Create User', function() {
+  describe('Create user succesly', function() {
+    it('should return true when the user is create', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
